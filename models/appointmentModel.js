87cc3935d68930e7a3f545590c6f6d68a40c2ce0
santoshallu1234 <santoshallu1234@gmail.com>
2024-10-18 00:18:40 +0530

    payed

--- models/appointmentModel.js
+++ models/appointmentModel.js
@@ -5,7 +5,7 @@ const AppointmentSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   therapist: { type: mongoose.Schema.Types.ObjectId, ref: 'Therapist', required: true },
   date: { type: Date, required: true },
-  status: { type: String, enum: ['pending', 'confirmed', 'completed','payed'], default: 'pending' },
+  status: { type: String, enum: ['pending', 'confirmed', 'completed','payed',], default: 'pending' },
   location: { type: String, default: 'Vatsalya Trust', required: true }
 });
 
